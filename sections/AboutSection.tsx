0c1a9d0af50fe7f042d7b437694c553ac7b6f14e
kigod0617 <gurumandv1123@gmail.com>
2024-09-20 19:14:50 +0100

    Rename filename

--- sections/AboutSection.tsx
+++ sections/AboutSection.tsx
@@ -9,7 +9,7 @@ import { useSection } from "context/section";
 import useOnScreen from "hooks/useOnScreen";
 import useScrollActive from "hooks/useScrollActive";
 
-import bitCoin from "../public/bitCoin.jpg";
+import bitCoin from "../public/bitcoin.jpg";
 import AboutBgSvg from "@/components/AboutBgSvg";
 import EduGroup from "@/components/EduGroup";
 
